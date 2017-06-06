d3cba1133076e609571e645c8f84ef99e3577daf
pradanadike <pradanadike5@gmail.com>
2017-06-06 19:31:28 +0700

    sudah

---
+++ src/rentaldvd/RentalDVD.java
@@ -0,0 +1,12 @@
+package rentaldvd;
+
+public class RentalDVD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
